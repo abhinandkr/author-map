f89d91bf55ef27329ce9279a9300dc392ac625f9
abhinandkr <abhinandkr1@gmail.com>
2022-08-03 11:58:26 +0530

    Calling author-server

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true,
+	},
+	'extends': [
+		'plugin:react/recommended',
+		'google',
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true,
+		},
+		'ecmaVersion': 'latest',
+		'sourceType': 'module',
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint',
+	],
+	'rules': {
+		'require-jsdoc': 0,
+		'indent': [1, 'tab'],
+		'no-tabs': 0,
+		'padded-blocks': 0,
+	},
+};
--- src/pages/PlaceDisplay.tsx
+++ src/pages/PlaceDisplay.tsx
@@ -8,15 +8,14 @@ type Props = {
 export default function PlaceDisplay(props: Props) {
 	const [birthPlace, setBirthPlace] = useState<string>();
 	useEffect(() => {
-		fetchBirthPlace(props.authorName)
-			.then((p: string | null) => {
-				if (p) {
-					setBirthPlace(p);
-				} else {
-					setBirthPlace('');
-				}
-			})
-			.catch((e) => console.log(e));
+		if (props.authorName) {
+			httpGet(`http://localhost:3000/api/authorPlace/author/${props.authorName}`)
+				.then((res) => res.data)
+				.then((val: string) => {
+					console.log(val);
+					setBirthPlace(val);
+				});
+		}
 	}, [props.authorName]);
 	return (
 		<div>
@@ -25,38 +24,19 @@ export default function PlaceDisplay(props: Props) {
 	);
 }
 
-async function fetchData(url: string) {
-	const res = await axios.get(url);
-	return res.data.d.results;
-}
+// function getOptions(verb: string) {
+// 	const options = {
+// 		dataType: 'json',
+// 		method: verb,
+// 		headers: {
+// 			'Accept': 'application/json',
+// 			'Content-Type': 'application/json',
+// 		},
+// 	};
+// 	return options;
+// }
 
-async function fetchBirthPlace(author: string) {
-	if (!author) {
-		return null;
-	}
-	const birthplaceObj = await getData(generateUri(author), 'birthPlace');
-	if (!birthplaceObj) {
-		return null;
-	}
-	if (!(birthplaceObj instanceof Object)) {
-		return birthplaceObj;
-	}
-	return birthplaceObj['__deferred']['uri']
-		.split('/')
-		.pop()
-		.replaceAll('_', ' ');
-}
-
-async function getData(uri: string, property: string) {
-	const birthPlaceData = await fetchData(uri);
-	if (!birthPlaceData || birthPlaceData.length === 0) {
-		return null;
-	}
-	const birthplaceObj = birthPlaceData[0][`http://dbpedia.org/property/${property}`];
-	return birthplaceObj;
-}
-
-function generateUri(str: string) {
-	const name = str.replaceAll(' ', '_');
-	return `https://dbpedia.org/data/${name}.jsod`;
+function httpGet(path: string) {
+	// return fetch(path, getOptions('GET'));
+	return axios.get(path);
 }
